93c15065e9266d719ba1e6ee1a2c958fd897c51d
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-05-08 16:10:11 +0200

    refactor

--- src/main/scala/RotationalCipher.scala
+++ src/main/scala/RotationalCipher.scala
@@ -1,4 +1,4 @@
 object RotationalCipher:
    def rotate(s: String, i: Int): String =
-      def switch(c: Char) = if c.isLower then 'a' else 'A'
-      s.map(c => if !c.isLetter then c else ((c - switch(c) + i) % 26 + switch(c)).toChar)
+      def f(c: Char) = if c >= 'a' then 'a' else 'A'
+      s.map(c => if c.isLetter then ((c - f(c) + i) % 26 + f(c)).toChar else c)
