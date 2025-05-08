726a4f2c7f69af0aee0efa4c5a7ce875f3363c6c
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-05-08 15:58:05 +0200

    refactor

--- src/main/scala/RotationalCipher.scala
+++ src/main/scala/RotationalCipher.scala
@@ -1,9 +1,4 @@
 object RotationalCipher:
    def rotate(s: String, i: Int): String =
-      val rot = if i > 25 then i - 26 else i
-
-      for c <- s
-      yield c match
-         case c1 if (97 to 122).contains(c1) => if (c1 + rot) > 122 then (c1 + rot - 26).toChar else (c1 + rot).toChar
-         case c2 if (65 to 90).contains(c2)  => if (c2 + rot) > 90 then (c2 + rot - 26).toChar else (c2 + rot).toChar
-         case _                              => c
+      def switch(c: Char) = if c.isLower then 'a' else 'A'
+      s.map(c => if !c.isLetter then c else ((c - switch(c) + i) % 26 + switch(c)).toChar)
